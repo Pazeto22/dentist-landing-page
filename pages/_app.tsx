deec9262e4545fea4cd7a97276fa1e1df9587ce2
Pazeto22 <gustavopazeto95@gmail.com>
2022-09-28 20:31:12 -0300

    Feat: Fontes, helpers e cores

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,11 @@ import dynamic from "next/dynamic";
 import React from "react";
 import "../styles/globals.scss";
 
-// import "@fontsource/roboto/200.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
-// import "@fontsource/roboto/600.css";
 import "@fontsource/roboto/700.css";
-import "@fontsource/roboto/900.css";
+import "@fontsource/lora/700.css";
 
 function App({ Component, pageProps }: AppProps) {
   return (
